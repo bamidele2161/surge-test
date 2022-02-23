8b6999319570d60dbd8c85a916e31411b4370eea
bamidele2161 <59539644+bamidele2161@users.noreply.github.com>
2022-02-23 13:38:38 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
     email: "",
     password: "",
     select: "",
+    check: false,
   }
+
   const [modal, setModal] = useState(false); // setting state for modal (inital state is false)
   const [formValues, setFormValues] = useState(initialValues); // setting state for values
   const [formErrors, setFormErrors] = useState({}); //seting state for errors
@@ -25,13 +27,15 @@ function App() {
     setFormErrors(validate(formValues));
     setModal(!modal);
     setIsSubmit(true);
+    
   }
 
-  useEffect(() => {
-    if(Object.keys(formErrors).length===0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formValues, formErrors, isSubmit]); //useEffect dependencies
+  // useEffect(() => {
+  //   if(Object.keys(formErrors).length===0 && isSubmit) {
+  //     console.log(formValues);
+  //   }
+  // }, [formValues, formErrors, isSubmit]); //useEffect dependencies
+
 
   const validate = (values) => {
     const errors = {};
@@ -41,6 +45,9 @@ function App() {
     }
     if (!values.lastname) { // validating last name
       errors.lastname = "Last Name is required!";
+    }
+    if (!values.check) { // validating last name
+      errors.check = "Agree to KJK Africa's Terms and Conditions";
     }
     if (!values.email) { // validating email address
       errors.email = "Email is required!";
@@ -68,6 +75,7 @@ function App() {
   }
   return (
     <div className="app-container">
+
       <div className="first-container">
         <p className="brand-name">Surge</p>
         <h1 className="about-heading">Let us build Africa's next biggest products together</h1>
@@ -80,9 +88,11 @@ function App() {
         </ul>
         <a href='/' className="sign-up-link">Sign up and be a step closer to joining our tech base.</a>
       </div>
+
       <div className="second-container">
         <h1 className="form-heading">KJK.Africa Tech Talents</h1>
         <h3 className="form-sub-heading">Apply to join our Ecosystem</h3>
+        
         <form className="form-container">
           <div className="name-div">
             <div className="input-items">
@@ -132,9 +142,8 @@ function App() {
                   value={formValues.select}
                 >
                   <option>Select your City</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
+                  <option value="1">Benin</option>
+                  <option value="2">Ibadan</option>
                 </select>
                 <p className="error-paragraph">{formErrors.select}</p>
             </div>
@@ -154,12 +163,28 @@ function App() {
 
           <div className="form-others">
             <div className="checkbox">
-              <input type="checkbox" className="check-box" /> Agree to KJK Africa's Terms and Conditions?
+              <input 
+                type="checkbox" 
+                className="check-box" 
+                onChange={handleChange} 
+                name="check"
+                value="check"
+              /> Agree to KJK Africa's Terms and Conditions?
+              <p className="error-paragraph">{formErrors.check}</p>
             </div>
-            <button type="submit" className="submit-btn" onClick={handleSubmit}>Sign Up</button>
+
+            <button 
+              type="submit" 
+              className="submit-btn" 
+              onClick={handleSubmit}
+            >
+              Sign Up
+            </button>
+            
             <div>
               Already have an account? <a href="/" className="login-link">LOG IN</a>
             </div>
+
           </div>
         </form>
         {
